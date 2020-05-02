2eddd39727209bb698a612f9cc6f2bf6909c822f
Shashank <mail2shashank1683@gmail.com>
2020-05-02 22:35:47 +0530

    updated example for Sentiment Analysis on IMDB Movie Reviews

--- Sentiment Analysis of IMDB Movie Reviews.py
+++ Sentiment Analysis of IMDB Movie Reviews.py
@@ -1,44 +1,35 @@
 import nltk
 import pandas as pd
+import sklearn
 from bs4 import BeautifulSoup
 
 from nltk import ToktokTokenizer
-import re, string, unicodedata
+import re
 
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import LabelBinarizer
 
 import warnings
+
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 20)
 
-imdb_data = pd.read_csv('dataset/IMDB Dataset.csv')
-#print(imdb_data.shape)
-#print(imdb_data.head(10))
-
+imdb_data_set = pd.read_csv('dataset/IMDB Dataset.csv')
 # Summary of the dataset
-#print(imdb_data.describe())
+print(imdb_data_set.describe())
 
-# sentiment count
-imdb_data['sentiment'].value_counts()
+# selecting only first 1000 row so that can compare and verify the results
+imdb_data = imdb_data_set.iloc[:]
+print(imdb_data.keys())
 
-# split the dataset
-# train dataset
-train_reviews = imdb_data.review[:40000]
-train_sentiments = imdb_data.sentiment[:40000]
-
-# test dataset
-test_reviews = imdb_data.review[40000:]
-test_sentiments = imdb_data.sentiment[40000:]
-
-# print(train_reviews.shape, train_sentiments.shape)
-# print(test_reviews.shape, test_sentiments.shape)
-
-# Tokenization of text
-tokenizer = ToktokTokenizer()
+X = imdb_data['review']
+Y = imdb_data.drop('review', axis=1)
 
 
 # Removing the html strips
@@ -59,19 +50,11 @@ def denoise_text(text):
     return text
 
 
-# Apply function on review column
-imdb_data['review'] = imdb_data['review'].apply(denoise_text)
-
-
 # Define function for removing special characters
 def remove_special_characters(text, remove_digits=True):
     pattern = r'[^a-zA-z0-9\s]'
     text = re.sub(pattern, '', text)
     return text
-
-
-# Apply function on review column
-imdb_data['review'] = imdb_data['review'].apply(remove_special_characters)
 
 
 # Stemming the text
@@ -81,9 +64,8 @@ def simple_stemmer(text):
     return text
 
 
-# Apply function on review column
-imdb_data['review'] = imdb_data['review'].apply(simple_stemmer)
-imdb_data.head(10)
+# Tokenization of text
+tokenizer = ToktokTokenizer()
 
 # set stopwords to english
 stop_words = set(stopwords.words('english'))
@@ -102,124 +84,85 @@ def remove_stopwords(text, is_lower_case=False):
 
 
 # Apply function on review column
-imdb_data['review'] = imdb_data['review'].apply(remove_stopwords)
+X = X.apply(denoise_text)
 
-# normalized train reviews
-norm_train_reviews = imdb_data.review[:40000]
-print(norm_train_reviews)
-# print(norm_train_reviews[0])
-# convert dataframe to string
-# norm_train_string=norm_train_reviews.to_string()
-# Spelling correction using Textblob
-# norm_train_spelling=TextBlob(norm_train_string)
-# norm_train_spelling.correct()
-# Tokenization using Textblob
-# norm_train_words=norm_train_spelling.words
-# norm_train_words
+# Apply function on review column
+X = X.apply(remove_special_characters)
 
-# Normalized test reviews
-norm_test_reviews = imdb_data.review[40000:]
-norm_test_reviews
-# print(norm_test_reviews[45005])
-##convert dataframe to string
-# norm_test_string=norm_test_reviews.to_string()
-# spelling correction using Textblob
-# norm_test_spelling=TextBlob(norm_test_string)
-# print(norm_test_spelling.correct())
-# Tokenization using Textblob
-# norm_test_words=norm_test_spelling.words
-# norm_test_words
+# Apply function on review column
+X = X.apply(simple_stemmer)
 
-'''
-Bags of words model
-It is used to convert text documents to numerical vectors or bag of words.
-'''
-
-# Count Vectorizer for bag of words
-cv = CountVectorizer()
-# transformed train reviews
-cv_train_reviews = cv.fit_transform(norm_train_reviews)
-# transformed test reviews
-cv_test_reviews = cv.transform(norm_test_reviews)
-
-print('BOW_cv_train:', cv_train_reviews.shape)
-print('BOW_cv_test:', cv_test_reviews.shape)
-
-'''
-Term Frequency-Inverse Document Frequency model (TFIDF)
-It is used to convert text documents to matrix of tfidf features.
-'''
-# Tfidf vectorizer
-tv = TfidfVectorizer()
-# transformed train reviews
-tv_train_reviews = tv.fit_transform(norm_train_reviews)
-# transformed test reviews
-tv_test_reviews = tv.transform(norm_test_reviews)
-print('Tfidf_train:', tv_train_reviews.shape)
-print('Tfidf_test:', tv_test_reviews.shape)
+# Apply function on review column
+X = X.apply(remove_stopwords)
 
 # labeling the sentient data
-lb = LabelBinarizer()
+label_binarizer = LabelBinarizer()
+
 # transformed sentiment data
-sentiment_data = lb.fit_transform(imdb_data['sentiment'])
-print(sentiment_data.shape)
+Y = label_binarizer.fit_transform(Y)
 
-# Spliting the sentiment data
-train_sentiments = sentiment_data[:40000]
-test_sentiments = sentiment_data[40000:]
-print('----------------------------')
-'''
-Modelling the dataset
-Let us build logistic regression model for both bag of words and tfidf features.
-'''
-# training the model
-lr = LogisticRegression(solver='lbfgs')
+X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.20, random_state=5)
 
-# Fitting the model for Bag of words
-lr_bow = lr.fit(cv_train_reviews, train_sentiments)
-print(lr_bow)
-print('----------------------------')
-# Fitting the model for tfidf features
-lr_tfidf = lr.fit(tv_train_reviews, train_sentiments)
-print(lr_tfidf)
-print('----------------------------')
-'''
-Logistic regression model performane on test dataset.
-'''
+# Count Vectorizer for bag of words
+count_vectorizer = CountVectorizer()
 
-# Predicting the model for bag of words
-lr_bow_predict = lr.predict(cv_test_reviews)
-print(lr_bow_predict)
-##Predicting the model for tfidf features
-lr_tfidf_predict = lr.predict(tv_test_reviews)
-print(lr_tfidf_predict)
+# transformed train reviews
+count_vectorizer_train_reviews = count_vectorizer.fit_transform(X_train)
 
-'''
-Accuracy of the model
-'''
-# Accuracy score for bag of words
-lr_bow_score = accuracy_score(test_sentiments, lr_bow_predict)
-print("lr_bow_score :", lr_bow_score)
-# Accuracy score for tfidf features
-lr_tfidf_score = accuracy_score(test_sentiments, lr_tfidf_predict)
-print("lr_tfidf_score :", lr_tfidf_score)
+# transformed test reviews
+count_vectorizer_test_reviews = count_vectorizer.transform(X_test)
 
-# Classification report for bag of words
-lr_bow_report = classification_report(test_sentiments, lr_bow_predict, target_names=['Positive', 'Negative'])
-print(lr_bow_report)
+# Tfidf vectorizer
+tfidf_vectorizer = TfidfVectorizer()
 
-# Classification report for tfidf features
-lr_tfidf_report = classification_report(test_sentiments, lr_tfidf_predict, target_names=['Positive', 'Negative'])
-print(lr_tfidf_report)
+# transformed train reviews
+tfidf_vectorizer_train_reviews = tfidf_vectorizer.fit_transform(X_train)
 
-# confusion matrix for bag of words
-cm_bow = confusion_matrix(test_sentiments, lr_bow_predict, labels=[1, 0])
-print(cm_bow)
-# confusion matrix for tfidf features
-cm_tfidf = confusion_matrix(test_sentiments, lr_tfidf_predict, labels=[1, 0])
-print(cm_tfidf)
+# transformed test reviews
+tfidf_vectorizer_test_reviews = tfidf_vectorizer.transform(X_test)
 
-'''
-create list of algo and add logic to processing
-understand confusion_matrix and try to replace with accuracy 
-'''
+classifiers_list = [
+    LogisticRegression(solver='lbfgs'),
+    SGDClassifier(),
+    MultinomialNB()
+]
+
+for classifier in classifiers_list:
+    print('============================================================')
+    # Fitting the model for Bag of words
+    classifier.fit(count_vectorizer_train_reviews, Y_train)
+
+    # Fitting the model for tfidf features
+    classifier.fit(tfidf_vectorizer_train_reviews, Y_train)
+
+    # Predicting the model for bag of words
+    bow_prediction = classifier.predict(count_vectorizer_test_reviews)
+    print(bow_prediction)
+
+    # Predicting the model for tfidf features
+    tfidf_prediction = classifier.predict(tfidf_vectorizer_test_reviews)
+    print(tfidf_prediction)
+
+    # Accuracy score for bag of words
+    accuracy_score_bow = accuracy_score(Y_test, bow_prediction)
+    print("Accuracy score for bag of words :", accuracy_score_bow)
+
+    # Accuracy score for tfidf features
+    accuracy_score_tfidf = accuracy_score(Y_test, tfidf_prediction)
+    print("Accuracy score for tfidf features :", accuracy_score_tfidf)
+
+    # Classification report for bag of words
+    # lr_bow_report = classification_report(test_sentiments, lr_bow_predict, target_names=['Positive', 'Negative'])
+    # print(lr_bow_report)
+
+    # Classification report for tfidf features
+    # lr_tfidf_report = classification_report(test_sentiments, lr_tfidf_predict, target_names=['Positive', 'Negative'])
+    # print(lr_tfidf_report)
+
+    # confusion matrix for bag of words
+    confusion_matrix_bow = confusion_matrix(Y_test, bow_prediction, labels=[1, 0])
+    print('confusion matrix for bag of words \n', confusion_matrix_bow)
+
+    # confusion matrix for tfidf features
+    confusion_matrix_tfidf = confusion_matrix(Y_test, tfidf_prediction, labels=[1, 0])
+    print('confusion matrix for bag of words \n', confusion_matrix_tfidf)
