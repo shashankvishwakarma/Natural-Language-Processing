abf862b0138f7bbfb92863e5b93178637f66ff62
Shashank <mail2shashank1683@gmail.com>
2020-05-17 22:41:44 +0530

    udpated SMS Spam Classification

--- README.md
+++ README.md
@@ -9,3 +9,25 @@ This is Natural Language Processing tutorial, using the Natural Language Toolkit
 https://www.kaggle.com/lakshmi25npathi/sentiment-analysis-of-imdb-movie-reviews
 
  
+# what is TF-IDF?
+TF-IDF stands for term frequency-inverse document frequency, and the tf-idf weight is a weight often used in information retrieval and text mining. This weight is a statistical measure used to evaluate how important a word is to a document in a collection or corpus. The importance increases proportionally to the number of times a word appears in the document but is offset by the frequency of the word in the corpus. Variations of the tf-idf weighting scheme are often used by search engines as a central tool in scoring and ranking a document's relevance given a user query.
+
+One of the simplest ranking functions is computed by summing the tf-idf for each query term; many more sophisticated ranking functions are variants of this simple model.
+
+Typically, the tf-idf weight is composed by two terms: the first computes the normalized Term Frequency (TF), aka. the number of times a word appears in a document, divided by the total number of words in that document; the second term is the Inverse Document Frequency (IDF), computed as the logarithm of the number of the documents in the corpus divided by the number of documents where the specific term appears.
+
+TF: Term Frequency, which measures how frequently a term occurs in a document. Since every document is different in length, it is possible that a term would appear much more times in long documents than shorter ones. Thus, the term frequency is often divided by the document length (aka. the total number of terms in the document) as a way of normalization:
+
+TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
+
+IDF: Inverse Document Frequency, which measures how important a term is. While computing TF, all terms are considered equally important. However it is known that certain terms, such as "is", "of", and "that", may appear a lot of times but have little importance. Thus we need to weigh down the frequent terms while scale up the rare ones, by computing the following:
+
+IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
+
+See below for a simple example.
+
+Example:
+
+Consider a document containing 100 words wherein the word cat appears 3 times.
+
+The term frequency (i.e., tf) for cat is then (3 / 100) = 0.03. Now, assume we have 10 million documents and the word cat appears in one thousand of these. Then, the inverse document frequency (i.e., idf) is calculated as log(10,000,000 / 1,000) = 4. Thus, the Tf-idf weight is the product of these quantities: 0.03 * 4 = 0.12.
--- SMS Spam Classification.py
+++ SMS Spam Classification.py
@@ -1,8 +1,12 @@
-#import modules
+# https://github.com/KunalArora/SpamDetection-NLP-UCIData/blob/master/SpamDetection-NLP.ipynb
+# import modules
 import pandas as pd
 import numpy as np
+import string
 
 # Set ipython's max row display
+from nltk.corpus import stopwords
+
 pd.set_option('display.max_row', 1000)
 
 # Set iPython's max column width to 50
@@ -14,7 +18,7 @@ pd.set_option('display.max_colwidth', -1)
 # load data and view data
 sms_dataset = pd.read_csv('./dataset/spam.csv', sep=',', encoding='Latin-1')
 print(sms_dataset.shape)
-print(sms_dataset.head())
+#print(sms_dataset.head())
 
 # Remove empty columns and give existing columns meaningful names.¶
 # drop meaningless columns
@@ -22,94 +26,124 @@ sms_dataset = sms_dataset.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=
 
 # Give meaningful names for exist columns
 sms_dataset.columns = ['label', 'texts']
+print(sms_dataset.describe())
+print(sms_dataset.shape)
+#print(sms_dataset.head(5))
 
-# lowering case characters and stripping punctuation of texts
-from string import punctuation
-def preprocess1 (str):
-    """ str --> str
-    lower case and strip punctuations of a string
-    """
-    for p in list(punctuation):
-        str = str.lower().replace(p, '')
-    return str
-sms_dataset['texts'] = sms_dataset.texts.apply(preprocess1)
+# group by to use describe by label, this way we can begin to think about the features that separate ham and spam!
+# print(sms_dataset.groupby('label').describe())
 
-# creating word count feature as "word_count"
-def word_count (str):
-    """str --> int
-    return the number of words appeared in a string
+
+# Text Pre-processing
+def text_process(mess):
     """
-    return len(str.split())
-sms_dataset['word_count'] = sms_dataset.texts.apply(word_count)
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Returns a list of the cleaned text
+    """
+    # Check characters to see if they are in punctuation
+    no_punctuation = [char for char in mess if char not in string.punctuation]
+
+    # Join the characters again to form the string.
+    no_punctuation = ''.join(no_punctuation)
+
+    # Now just remove any stopwords
+    return [word for word in no_punctuation.split() if word.lower() not in stopwords.words('english')]
 
 # creating character count feature as "char_count"
-def char_count (str):
-    """ str --> int
-    return the number of character count of a string
-    """
-    return len(list(str))
-sms_dataset['char_count'] = sms_dataset.texts.apply(char_count)
+sms_dataset['char_count'] = sms_dataset.texts.apply(len)
+#sms_dataset['texts'] = sms_dataset['texts'].apply(text_process)
 
-# view new data set
-print(sms_dataset.shape)
-print(sms_dataset.head())
+# Vectorization
+from sklearn.feature_extraction.text import CountVectorizer
+# Might take awhile...
+bow_transformer = CountVectorizer(analyzer=text_process).fit(sms_dataset['texts'])
+# Print total number of vocab words
+print(len(bow_transformer.vocabulary_))
 
-# Statistical Inferance for Word Count.
+# Let's take one text message and get its bag-of-words counts as a vector, putting to use our new bow_transformer:
+text_4 = sms_dataset['texts'][3]
+print('Text at index 4 {}'.format(text_4))
+
+# Now let's see its vector representation:
+bow_4 = bow_transformer.transform([text_4])
+print(bow_4)
+print(bow_4.shape)
+
 '''
-If we can confirm the ham sample and spam sample come from populations that are normally distributed, 
-we can use t-test to test if spam messages has significantly greater word count than ham messages.
-There's a Shapiro-Wilk test to test for normality. 
-If p-value is less than 0.05, then there's a low chance that the distribution is normal.
+output of above shows, that there are seven unique words in message number 4 (after removing common stop words). 
+Two of them appear twice, the rest only once. 
 '''
-ham_word_count = sms_dataset.loc[sms_dataset.label=='ham']['word_count']
-spam_word_count = sms_dataset.loc[sms_dataset.label=='spam']['word_count']
 
-from scipy import stats
-print ("Shapiro-Wilk test results for ham sample:", stats.shapiro(ham_word_count))
-print ("Shapiro-Wilk test results for spam sample", stats.shapiro(spam_word_count))
-print ('mean-ham:', ham_word_count.mean())
-print ('mean-spam:', spam_word_count.mean())
-print('stand-deviation-ham:', ham_word_count.std())
-print('stand-deviation-spam:', spam_word_count.std())
-print('effect size - mean difference:', ham_word_count.mean() - spam_word_count.mean())
+# Let's go ahead and check and confirm which ones appear twice:
+print(bow_transformer.get_feature_names()[3996])
+print(bow_transformer.get_feature_names()[9445])
 
-# Cohan's D
-SS_ham = sum(np.square(i-ham_word_count.mean()) for i in ham_word_count)
-SS_spam = sum(np.square(i-ham_word_count.mean()) for i in spam_word_count)
-pooled_variance = (SS_ham + SS_spam) / (len(ham_word_count) + len(spam_word_count) - 2)
-cohans_D = (ham_word_count.mean() - spam_word_count.mean()) / np.sqrt(pooled_variance)
-print("effect size - Cohan's D:", cohans_D)
+'''
+Now we can use .transform on our Bag-of-Words (bow) transformed object and transform the entire DataFrame of messages. 
+Let's go ahead and check out how the bag-of-words counts for the entire SMS corpus is a large, sparse matrix:
+'''
+texts_bow = bow_transformer.transform(sms_dataset['texts'])
+print('Shape of Sparse Matrix: ', texts_bow.shape)
+print('Amount of Non-Zero occurrences: ', texts_bow.nnz)
 
-# Logistic Regression
-# dummy code variable
-sms_dataset = pd.get_dummies(sms_dataset, columns=['label'])
+sparsity = (100.0 * texts_bow.nnz / (texts_bow.shape[0] * texts_bow.shape[1]))
+print('sparsity: {}'.format(round(sparsity)))
 
-# Identify explaining variables and target variable
-y = sms_dataset[['label_spam']]
-X = sms_dataset[['word_count']]
+from sklearn.feature_extraction.text import TfidfTransformer
 
-# split data into training and test data sets
+tfidf_transformer = TfidfTransformer().fit(texts_bow)
+tfidf4 = tfidf_transformer.transform(bow_4)
+print('tfidf4 is',tfidf4)
+
+# We'll check what is the IDF (inverse document frequency) of the word "u" and of word "university"?
+print(tfidf_transformer.idf_[bow_transformer.vocabulary_['u']])
+print(tfidf_transformer.idf_[bow_transformer.vocabulary_['university']])
+
+# To transform the entire bag-of-words corpus into TF-IDF corpus at once:
+texts_tfidf = tfidf_transformer.transform(texts_bow)
+print(texts_tfidf.shape)
+
+# Training a model
+# With messages represented as vectors, we can finally train our spam/ham classifier. Now we can actually use almost any sort of classification algorithms.
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(texts_tfidf, sms_dataset['label'])
+print('predicted:', spam_detect_model.predict(tfidf4)[0])
+print('expected:', sms_dataset.label[3])
+
+# Model Evaluation
+#Now we want to determine how well our model will do overall on the entire dataset. Let's begin by getting all the predictions:
+all_predictions = spam_detect_model.predict(texts_tfidf)
+print(all_predictions)
+
+from sklearn.metrics import classification_report
+print (classification_report(sms_dataset['label'], all_predictions))
+
+
+# Train Test Split
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# apply LogisticRegression classifier
-from sklearn.linear_model import LogisticRegression
-lg = LogisticRegression(class_weight='balanced').fit(X_train, y_train)
-print (lg.coef_)
-print('training set score obtained Logistic Regression: {:.2f}'.format(lg.score(X_train, y_train)))
-print('test set score obtained Logistic Regression: {:.2f}'.format(lg.score(X_test, y_test)))
+X_train, X_test, Y_train, Y_test = train_test_split(sms_dataset['texts'], sms_dataset['label'], test_size=0.2)
+print(len(X_train), len(X_test), len(Y_train) + len(Y_test))
 
-prediction = lg.predict(X_test)
+# Creating a Data Pipeline
+'''Let's run our model again and then predict off the test set. 
+We will use SciKit Learn's pipeline capabilities to store a pipeline of workflow. 
+This will allow us to set up all the transformations that we will do to the data for future use. 
+'''
 
-from sklearn import metrics
-accuracy = metrics.accuracy_score(y_test,prediction)
-print('Accuracy {}'.format(accuracy))
+from sklearn.pipeline import Pipeline
+pipeline = Pipeline([
+    ('bow', CountVectorizer(analyzer=text_process)),  # strings to token integer counts
+    ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
+    ('classifier', MultinomialNB()),  # train on TF-IDF vectors w/ Naive Bayes classifier
+])
 
-confusion_matrix = metrics.confusion_matrix(y_test, prediction)
-print('confusion_matrix {}'.format(confusion_matrix))
-
-classification_report = metrics.classification_report(y_test, prediction)
-print('classification_report {}'.format(classification_report))
-
-
-
+'''
+Now we can directly pass message text data and the pipeline will do our pre-processing for us! 
+'''
+pipeline.fit(X_train,Y_train)
+predictions = pipeline.predict(X_test)
+print(classification_report(predictions,Y_test))
